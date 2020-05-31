733c740f917e8b81f4cd454705963c95ac18c4ed
Julius Cesar Machado Dantas <juliusmdantas@hotmail.com>
2020-05-31 12:19:15 -0300

    Aula de JavaScript

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+/*var nome= "Nome"
+alert("Estudo de JS " + nome);
+
+var lista = ["Maçã", "Pêra", "Laranja"];
+
+lista.push("Uva");
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join("|"));
+alert(lista);
+
+var idade = prompt("Qual a sua idade?")
+//var idade = 18;
+if (idade > 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de Idade");
+};
+
+var count = 0;
+while (count < 5){
+    console.log(count);
+    count = count + 1;
+    alert(count);
+};
+
+var conta;
+FormData(conta, conta <=5, conta++);{
+    alert(conta);
+};
+
+var d = new Date();
+alert(d);
+
+var n1 = prompt("Digite o primeiro valor ");
+var n2 = prompt("Digite o segundo valor ");
+function soma(n1,n2){
+    return n1 + n2;
+
+}
+
+alert(soma(n1,n2));*/
+
+
+
